8e12551d260acb9b01ded5a332333edac08e1757
Lyriraki <Luckyarif21@gmail.com>
2020-10-19 22:49:09 +0700

    Fisrt

---
+++ assets/javascript/javascript.js
@@ -0,0 +1,20 @@
+// AOS
+AOS.init();
+
+// menuBtn
+let menuBtn = document.getElementById("menuBtn");
+let sideNav = document.getElementById("sideNav");
+
+sideNav.style.right = "-250px";
+menuBtn.onclick = function () {
+  if (sideNav.style.right == "-250px") {
+    sideNav.style.right = "0";
+  } else {
+    sideNav.style.right = "-250px";
+  }
+};
+
+var scroll = new SmoothScroll('a[href*="#"]', {
+  speed: 800,
+  speedDuration: true,
+});
